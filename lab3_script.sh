b98ccccced3f1f2ba50f8d19a9b93fc0827637ea
timkan12 <thka1840@colorado.edu>
2020-02-07 11:30:16 -0700

    Script for lab 3

---
+++ lab3_script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Authors : Tim Kan and Bao Nguyends
+# Date: 2/7/2020
+
+#Problem 1 Code:
+#Make sure to document how you are solving each problem!
+echo "Enter the file name" 
+read  fileName
+echo "Enter what your are searching for"
+read search
+
+echo "the filename: $fileName"
+echo "the search: $search" 
+
+echo "Number of phone number:"
+grep -c '[0-9]\{3\}\-[0-9]\{3\}\-[0-9]\{4\}' regex_practice.txt
+echo "Number of phone number of 303 area code"
+grep  '303' regex_practice.txt
+grep "@geocities.com" regex_practice.txt >> email_results.txt
+git add email_results.txt
+git commit -m  "Adding Email look up"
+git push origin master
